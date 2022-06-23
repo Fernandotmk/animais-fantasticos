0b4f80249b31b6c99137190abfb7cdb5e084595c
“Fernandotmk” <fernando.tmk@hotmail.com>
2022-06-23 13:55:22 -0300

    Accordion List

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,19 +1,25 @@
-// lista faq
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    "[data-accordion='accordion'] dt"
-  );
-  const activeClass = 'ativo';
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  addAcordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      // deixa o primeiro item ativado
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAcordionEvent();
+    }
   }
 }
