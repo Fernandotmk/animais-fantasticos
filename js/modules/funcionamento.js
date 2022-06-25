d992aa86511d7b087dcd929719ea73525124322f
“Fernandotmk” <fernando.tmk@hotmail.com>
2022-06-25 09:36:12 -0300

    refatorado horario de funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,31 +1,43 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector('[data-semana]');
-  const diaSemana = funcionamento.dataset.semana.split(',').map(Number); // split converte em array com a virgula como separador e o map converte o string em number
-  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-  const semanaAberto = diaSemana.indexOf(diaAgora) !== -1;
-  // pegou o array do dia Semana. [1, 2, 3, 4, 5] sendo segunda, terça até sexta , verificou o index(posição do dia agora), se o dia agora nao estiver dentro do Index, vai retornar -1, entao retornando false, dizendo que esta fechado
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
-  // nesse caso pegou os dois itens do array de horario semana, o na primeira posição que é 8 e o segundo que é 18, entao se o horario agora estiver nesse intervalo estará como aberto
+  dadosFuncionamento() {
+    this.diaSemana = this.funcionamento.dataset.semana.split(',').map(Number); // split converte em array com a virgula como separador e o map converte o string em number
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(',')
+      .map(Number);
+  }
 
-  if (semanaAberto && horarioAberto) {
-    funcionamento.classList.add('aberto');
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
+
+  estaAberto() {
+    const semanaAberto = this.diaSemana.indexOf(this.diaAgora) !== -1;
+    // pegou o array do dia Semana. [1, 2, 3, 4, 5] sendo segunda, terça até sexta , verificou o index(posição do dia agora), se o dia agora nao estiver dentro do Index, vai retornar -1, entao retornando false, dizendo que esta fechado
+
+    const horarioAberto = this.horarioAgora >= this.horarioSemana[0] && this.horarioAgora < this.horarioSemana[1];
+    // nesse caso pegou os dois itens do array de horario semana, o na primeira posição que é 8 e o segundo que é 18, entao se o horario agora estiver nesse intervalo estará como aberto
+    return semanaAberto && horarioAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
   }
 }
-/*
-const agora = new Date();
-const futuro = new Date('Dec 24 2022 23:59');
-
-function converterEmDias(tempo) {
-  return tempo / (24 * 60 * 60 * 1000) // 24hrs tem 60mim , cada min tem 60 segudos, cada segundo tem 1000 milesegundos
-}
-
-const diasAgora = converterEmDias(agora.getTime());
-const diasFuturo = converterEmDias(futuro.getTime());
-
-const faltam = Math.floor(diasAgora - diasFuturo);
-
-console.log(faltam); */
